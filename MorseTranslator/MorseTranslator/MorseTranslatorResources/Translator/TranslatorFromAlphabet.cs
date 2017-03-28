9637b8262c8794fbeb21f482412507b338055e03
ikolodziejczyk <ikolodziejczyk@CFXCN12>
2017-03-28 15:57:30 +0200

    Add major methots to TranslatorFromAplhabet class

--- MorseTranslator/MorseTranslator/MorseTranslatorResources/Translator/TranslatorFromAlphabet.cs
+++ MorseTranslator/MorseTranslator/MorseTranslatorResources/Translator/TranslatorFromAlphabet.cs
@@ -8,5 +8,41 @@ namespace MorseTranslator.MorseTranslatorResources.Translator
 {
     class TranslatorFromAlphabet
     {
+        MorseCodeAlphabet MorseAlphabet = new MorseCodeAlphabet();
+
+        public string Translate(string textToTranslate)
+        {
+            return null;
+        }
+
+        public Boolean IsTextProperToTranslate(string text)
+        {
+            return false;
+        }
+
+        private string FormatText(string text)
+        {
+            return text.Trim().ToUpper();
+        }
+
+        private void CheckIfTextConsistOnlyLettersAndNumbers(string text)
+        {
+            text = FormatText(text);
+
+            foreach(char letter in text)
+            {
+                if (!CheckIfCharIsLetterOrDigit(letter))
+                {
+
+                }
+            }
+
+        }
+
+        private Boolean CheckIfCharIsLetterOrDigit(char chr)
+        {
+            return Char.IsLetterOrDigit(chr);
+        }
+
     }
 }

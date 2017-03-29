4b5a79cfce9ed14379c808243f213ac71ce67fa3
ikolodziejczyk <ikolodziejczyk@CFXCN12>
2017-03-29 15:28:35 +0200

    Basic Form creation.

--- MorseTranslator/MorseTranslator/Form1.cs
+++ MorseTranslator/MorseTranslator/Form1.cs
@@ -19,6 +19,16 @@ namespace MorseTranslator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }

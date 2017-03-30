0e7ca236e37db09ab4c92b01a26af8eda4f8958f
ikolodziejczyk <ikolodziejczyk@CFXCN12>
2017-03-30 10:42:40 +0200

    Basics Methods added in TranslatorFromMorseCode class but not finished yet

--- MorseTranslator/MorseTranslator/MorseTranslatorResources/Test/TranslatorFromMorseCodeTest.cs
+++ MorseTranslator/MorseTranslator/MorseTranslatorResources/Test/TranslatorFromMorseCodeTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MorseTranslator.MorseTranslatorResources.Translator;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace MorseTranslator.MorseTranslatorResources.Test
 {
     class TranslatorFromMorseCodeTest
     {
+        TranslatorFromMorseCode translator = new TranslatorFromMorseCode();
+
+        [Test]
+        public void CheckIfIsMorseCodeTest()
+        {
+            Assert.True(translator.CheckIfIsMorseCode("..__ ..  __.____"));
+
+            Assert.False(translator.CheckIfIsMorseCode(".AbC!@#%^----___..___ _  &%$12456"));
+            
+        }
+
+        [Test]
+        public void TranslateTest()
+        {
+
+        }
     }
 }
--- MorseTranslator/MorseTranslator/MorseTranslatorResources/Translator/TranslatorFromMorseCode.cs
+++ MorseTranslator/MorseTranslator/MorseTranslatorResources/Translator/TranslatorFromMorseCode.cs
@@ -8,5 +8,55 @@ namespace MorseTranslator.MorseTranslatorResources.Translator
 {
     class TranslatorFromMorseCode
     {
+        public string  Translate(string morseCodeMessage)
+        {
+            
+        }
+
+        public bool CheckIfIsMorseCode(string textToCheck)
+        {
+            bool status = true;
+
+            foreach(char chr in textToCheck)
+            {
+                if (!CheckIfCharIsElementOfMorseCode(chr))
+                {
+                    status = false;
+                    break;
+                }
+            }
+            return status;
+        }
+
+        private bool CheckIfCharIsSpace(char chr)
+        {
+            return chr == ' ';
+                
+        }
+
+        private int GetIndexOfSpace(string text)
+        {
+            int index = -1;
+            for(int i = 0; i <= text.Length; i++)
+            {
+
+            }
+            return index
+        }
+
+        private bool CheckIfCharIsElementOfMorseCode(char chr)
+        {
+            bool status = true;
+
+            char[] morseCodeElements = { ' ', '.', '_' };
+
+            if (!morseCodeElements.Contains(chr))
+            {
+                status = false;
+            }
+
+            return status;
+        }
+
     }
 }
